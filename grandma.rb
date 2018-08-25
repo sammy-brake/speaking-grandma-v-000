ed78e0bc5234108a75c19f19e8ad508e52338806
Samantha brake <sammy.brake@gmail.com>
2018-08-25 16:25:24 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -19,6 +19,6 @@ def speak_to_grandma(phrase)
      elsif phrase == phrase.upcase && phrase != "I LOVE YOU GRANDMA!"
      return "NO, NOT SINCE 1938!"
    elsif phrase == "I LOVE YOU GRANDMA!"
-   return "I LOVE YOU TOO PUMPKIN"
+   return "I LOVE YOU TOO PUMPKIN!"
   end 
 end
